d71a339e023f359ff5c7d72055185387295b8edd
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2024-09-04 15:17:22 -0400

    Fixed Struct and moved test data one dir up

--- CSCI2210/Project 1/Coord2Dd.cs
+++ CSCI2210/Project 1/Coord2Dd.cs
@@ -13,5 +13,11 @@ namespace Battleship
     {
         public int x { get; set; }
         public int y { get; set; }
+
+        public Coord2D(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
