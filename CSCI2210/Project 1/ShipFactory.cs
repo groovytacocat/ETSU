aea3ee785b1a2d2d45e3e5cda469547fc09ebfbc
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2024-09-04 20:23:33 -0400

    Updated after office hours

--- CSCI2210/Project 1/ShipFactory.cs
+++ CSCI2210/Project 1/ShipFactory.cs
@@ -16,28 +16,26 @@ namespace Battleship
         {
             List<Ship> shipList = new List<Ship>();
 
-            using (FileStream shipFile = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            // TODO file permussuins stuff
+            using (StreamReader shipFile = new StreamReader(filePath))
             {
-                using (StreamReader sr = new(shipFile))
-                {
-                    string line;
+                string line;
 
-                    while ((line = sr.ReadLine()) != null)
+                while ((line = shipFile.ReadLine()) != null)
+                {
+                    if (line.StartsWith("#"))
                     {
-                        if (line.StartsWith("#"))
+                        continue;
+                    }
+                    else
+                    {
+                        try
                         {
-                            continue;
+                            shipList.Add(ParseShipString(line));
                         }
-                        else
+                        catch (Exception e)
                         {
-                            try
-                            {
-                                shipList.Add(ParseShipString(line));
-                            }
-                            catch (Exception e)
-                            {
-                                throw new Exception($"Unable to add ship: {e.Message}");
-                            }
+                            throw new Exception($"Unable to add ship: {e.Message}", e);
                         }
                     }
                 }
@@ -45,102 +43,85 @@ namespace Battleship
 
             return shipList.ToArray();
         }
+    
 
-        /// <inheritdoc/>
-        public Ship ParseShipString(string description)
+    /// <inheritdoc/>
+    public Ship ParseShipString(string description)
+    {
+        bool valid = VerifyShipString(description);
+
+        if (!valid) throw new Exception("Invalid Ship String");
+
+        string[] shipVals = description.Split(',');
+
+        int x = int.Parse(shipVals[3].Trim());
+        int y = int.Parse(shipVals[4].Trim());
+
+        Coord2D pos = new Coord2D(x, y);
+
+        DirectionType dir = shipVals[2].Trim().Equals("h") ? DirectionType.Horizontal : DirectionType.Vertical;
+
+        switch (shipVals[0].ToUpper())
         {
-            bool valid = VerifyShipString(description);
-
-            if (valid)
-            {
-                string[] shipVals = description.Split(',');
-
-                int x = int.Parse(shipVals[3].Trim());
-                int y = int.Parse(shipVals[4].Trim());
-
-                Coord2D pos = new Coord2D(x, y);
-
-                DirectionType dir = shipVals[2].Trim().Equals("h") ? DirectionType.Horizontal : DirectionType.Vertical;
-
-                switch (shipVals[0].ToUpper())
-                {
-                    case "CARRIER":
-                        return new Carrier(pos, dir);
-                    case "DESTROYER":
-                        return new Destroyer(pos, dir);
-                    case "SUBMARINE":
-                        return new Submarine(pos, dir);
-                    case "BATTLESHIP":
-                        return new Battleship(pos, dir);
-                    case "PATROL BOAT":
-                        return new PatrolBoat(pos, dir);
-                    default:
-                        throw new Exception("Invalid Ship Type");   //Left here due to "Not all code paths return a value" error even though it would fail before this point
-                }
-            }
-            else
-            {
-                throw new Exception("Invalid Ship String");
-            }
+            case "CARRIER":
+                return new Carrier(pos, dir);
+            case "DESTROYER":
+                return new Destroyer(pos, dir);
+            case "SUBMARINE":
+                return new Submarine(pos, dir);
+            case "BATTLESHIP":
+                return new Battleship(pos, dir);
+            case "PATROL BOAT":
+                return new PatrolBoat(pos, dir);
+            default:
+                throw new Exception("Invalid Ship Type");   //Left here due to "Not all code paths return a value" error even though it would fail before this point
         }
 
-        /// <inheritdoc/>
-        public bool VerifyShipString(string description)
+    }
+
+    /// <inheritdoc/>
+    public bool VerifyShipString(string description)
+    {
+        string pattern = @"^(Carrier|Battleship|Destroyer|Patrol Boat|Submarine), \d+, (v|h), \d+, \d+$";
+
+        bool valid = Regex.IsMatch(description, pattern, RegexOptions.IgnoreCase);
+
+        if (!valid) return false;
+
+        string[] vals = description.Split(",");
+        int len = int.Parse(vals[1].Trim());
+
+        if (len >= 6) throw new Exception($" {vals[0]} has an Invalid Ship Length");
+
+        if (vals[2].Trim().ToLower().Equals("h"))
         {
-            string pattern = @"^(Carrier|Battleship|Destroyer|Patrol Boat|Submarine), \d+, (v|h), \d+, \d+$";
-
-            bool valid = Regex.IsMatch(description, pattern, RegexOptions.IgnoreCase);
-
-            if (valid)
+            if (int.Parse(vals[3].Trim()) + len >= 10)
             {
-                string[] vals = description.Split(",");
-                int len = int.Parse(vals[1].Trim());
-
-                if(len < 6)
-                {
-                    if (vals[2].Trim().ToLower().Equals("h"))
-                    {
-                        if (int.Parse(vals[3].Trim()) + len > 10)
-                        {
-                            throw new Exception($" {vals[0]} has an Invalid Ship Position (Horizontal portion extends past grid)");
-                        }
-                        else
-                        {
-                            return true;
-                        }
-                    }
-                    else
-                    {
-                        if (int.Parse(vals[4].Trim()) + len > 10)
-                        {
-                            throw new Exception($" {vals[0]} has an Invalid Ship Position (Vertical portion extends past grid)");
-                        }
-                        else
-                        {
-                            return true;
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception($" {vals[0]} has an Invalid Ship Length");
-                }
+                throw new Exception($" {vals[0]} has an Invalid Ship Position (Horizontal portion extends past grid)");
             }
             else
             {
-                return false;
+                return true;
+            }
+        }
+        else
+        {
+            if (int.Parse(vals[4].Trim()) + len >= 10)
+            {
+                throw new Exception($" {vals[0]} has an Invalid Ship Position (Vertical portion extends past grid)");
+            }
+            else
+            {
+                return true;
             }
         }
     }
+    }
 }
+
 /*
     QUESTIONS:
-    What should the Parsing do if an invalid ship is parsed but the rest are valid?
-    * Currently ends as soon as exception thrown. Assume that is the correct behavior.
-    
     How to handle the nested try catch's? 
-
-    Are my class constructors ok/Is it ok to use virtual methods in my abstract class?
 
     How much data validation necessary?
  */
