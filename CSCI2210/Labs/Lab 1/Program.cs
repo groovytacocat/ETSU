3cab26833caf3ca7aa0d6ee3db59f5924e7f5ba8
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2024-09-10 19:10:52 -0400

    Finished the IList<T> implementation. Basically copied the C# Docs for the Enumerator/Enumerable stuff

--- CSCI2210/Labs/Lab 1/Program.cs
+++ CSCI2210/Labs/Lab 1/Program.cs
@@ -1,159 +1,230 @@
 ﻿using System;
 using System.Collections;
 
-namespace Sept10;
-
-//Generics, Lists, Interfaces
-class Program
+namespace Sept10
 {
-    static void Main(string[] args)
+    //Generics, Lists, Interfaces
+    class Program
     {
-        ArrayList<int> test = new();
-
-        test.Add(1);
-        test.Add(2);
-        test.Add(3);
-
-        for(int i = 0; i < test.Count; i++)
+        static void Main(string[] args)
         {
-            Console.WriteLine($"test[{i}]: {test[i]}");
-        }
+            ArrayList<int> test = new();
 
-        Console.WriteLine($"Contains(1): {test.Contains(1)}");
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
 
-        test.Insert(1, 5);
-
-        Console.WriteLine($"Test[1] should be 5. Test[1] is: {test[1]}");
-
-        Console.WriteLine($"Index of 5 is: {test.IndexOf(5)}");
-
-        test.RemoveAt(1);
-
-        Console.WriteLine($"Test[1] was 5. Been removed should now be 2. Test[1] is: {test[1]}");
-
-        test.Clear();
-
-        Console.WriteLine("Test Values are now: ");
-        for(int i = 0; i < test.Count; i++)
-        {
-            Console.WriteLine($"test[{i}]: {test[i]}");
-        }
-    }
-}
-
-class ArrayList<T> : IList<T>
-{
-    T[] guts = new T[0];
-
-    public int Count {get {return guts.Length;}}
-
-    public bool IsReadOnly => throw new NotImplementedException();
-
-    public T this[int index]
-    {
-        get
-        {
-            return guts[index];
-        }
-        set
-        {
-            guts[index] = value;
-        }
-    }
-
-    public void Add(T input)
-    {
-        Array.Resize(ref guts, guts.Length + 1);
-
-        guts[^1] = input;
-    }
-
-    public bool Remove(T input)
-    {
-        if(guts.Length == 0) return false;
-
-        for(int i = 0; i < guts.Length; i++)
-        {
-            if(guts[i].Equals(input))
+            for (int i = 0; i < test.Count; i++)
             {
-                for(int j = i; j < guts.Length - 1; j++)
+                Console.WriteLine($"test[{i}]: {test[i]}");
+            }
+
+            Console.WriteLine($"Contains(1): {test.Contains(1)}");
+
+            test.Insert(1, 5);
+
+            Console.WriteLine($"Test[1] should be 5. Test[1] is: {test[1]}");
+
+            Console.WriteLine($"Index of 5 is: {test.IndexOf(5)}");
+
+            test.RemoveAt(1);
+
+            Console.WriteLine($"Test[1] was 5. Been removed should now be 2. Test[1] is: {test[1]}");
+
+            test.Clear();
+
+            Console.WriteLine("Test Values are now: ");
+            for (int i = 0; i < test.Count; i++)
+            {
+                Console.WriteLine($"test[{i}]: {test[i]}");
+            }
+
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+
+            Console.WriteLine("Test values: (using foreach)");
+            foreach(int i in test)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    #region ArrayList
+    class ArrayList<T> : IList<T>
+    {
+        T[] guts = new T[0];
+
+        public int Count { get { return guts.Length; } }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public T this[int index]
+        {
+            get
+            {
+                return guts[index];
+            }
+            set
+            {
+                guts[index] = value;
+            }
+        }
+
+        public void Add(T input)
+        {
+            Array.Resize(ref guts, guts.Length + 1);
+
+            guts[^1] = input;
+        }
+
+        public bool Remove(T input)
+        {
+            if (guts.Length == 0) return false;
+
+            for (int i = 0; i < guts.Length; i++)
+            {
+                if (guts[i].Equals(input))
                 {
-                    guts[j] = guts[j + 1];
+                    for (int j = i; j < guts.Length - 1; j++)
+                    {
+                        guts[j] = guts[j + 1];
+                    }
+
+                    Array.Resize(ref guts, guts.Length - 1);
+                    return true;
                 }
-
-                Array.Resize(ref guts, guts.Length - 1);
-                return true;
             }
+
+            return false;
         }
 
-        return false;
-    }
-
-    public int IndexOf(T item)
-    {
-        for(int i = 0; i < guts.Length; i ++)
+        public int IndexOf(T item)
         {
-            if(guts[i].Equals(item))
+            for (int i = 0; i < guts.Length; i++)
             {
-                return i;
+                if (guts[i].Equals(item))
+                {
+                    return i;
+                }
             }
+
+            return -1;
         }
 
-        return -1;
-    }
-
-    public void Insert(int index, T item)
-    {
-        if(index < 0 || index >= guts.Length) throw new ArgumentOutOfRangeException("Invalid Index");
-
-        Array.Resize(ref guts, guts.Length + 1);
-
-        for(int j = guts.Length - 1; j > index; j--)
+        public void Insert(int index, T item)
         {
-            guts[j] = guts[j - 1];
-        }
+            if (index < 0 || index >= guts.Length) throw new ArgumentOutOfRangeException("Invalid Index");
 
-        guts[index] = item;
-    }
+            Array.Resize(ref guts, guts.Length + 1);
 
-    public void RemoveAt(int index)
-    {
-        Remove(guts[index]);
-    }
-
-    public void Clear()
-    {
-        guts = new T[0];
-    }
-
-    public bool Contains(T item)
-    {
-        for(int i = 0; i < guts.Length; i++)
-        {
-            if(guts[i].Equals(item))
+            for (int j = guts.Length - 1; j > index; j--)
             {
-                return true;
+                guts[j] = guts[j - 1];
+            }
+
+            guts[index] = item;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Remove(guts[index]);
+        }
+
+        public void Clear()
+        {
+            guts = new T[0];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < guts.Length; i++)
+            {
+                if (guts[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            for (int i = arrayIndex; i < guts.Length; i++)
+            {
+                array[i] = guts[i];
             }
         }
 
-        return false;
-    }
-
-    public void CopyTo(T[] array, int arrayIndex)
-    {
-        for(int i = arrayIndex; i < guts.Length; i++)
+        public IEnumerator<T> GetEnumerator()
         {
-            array[i] = guts[i];
+            return new ArrayListEnum<T>(guts);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator) GetEnumerator();
+        }
+    }
+    #endregion
+
+    public class ArrayListEnum<T> : IEnumerator<T>
+    {
+        public T[] values;
+
+        public int index = -1;
+
+        public T Current
+        {
+            get
+            {
+                try
+                {
+                    return values[index];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            return index < values.Length;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        public ArrayListEnum(T[] list)
+        {
+            values = list;
         }
     }
 
-    public IEnumerator<T> GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
 }
